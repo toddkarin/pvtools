137f96cd958168115d87f3bd1c78bccc0d7f6870
toddkarin <toddkarin@lbl.gov>
2019-03-20 16:40:10 -0700

    test mod

--- FlaskApp.wsgi
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.6
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/FlaskApp/")
-
-from FlaskApp import server as application
--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World App'),
+    html.H2('Hello Todd!'),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        options=[{'label': i, 'value': i} for i in ['Todd', 'NYC', 'MTL']],
         value='LA'
     ),
     html.Div(id='display-value')
