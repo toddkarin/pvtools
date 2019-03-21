a2c4f27edaec14b86ffed0132c642979e2e12c76
toddkarin <toddkarin@lbl.gov>
2019-03-21 15:24:37 -0700

    fixing download csv

--- string_length_calculator.py
+++ string_length_calculator.py
@@ -737,11 +737,13 @@ def update_graph(session_id, n_clicks, module_name, racking_model,
 
 
     # Make a directory for saving session files.
-    if not os.path.isdir(os.path.join('downloads',session_id)):
-        os.mkdir(os.path.join('downloads',session_id))
-    save_filename = os.path.join('downloads',session_id,'maxvoc_data.csv')
-    info_filename = os.path.join('downloads',session_id,'maxvoc_info.csv')
-    temp_filename = os.path.join('downloads', session_id, 'maxvoc_temp.pkl')
+    # if not os.path.isdir(os.path.join('downloads',session_id)):
+    #     os.mkdir(os.path.join('downloads',session_id))
+    save_filename = os.path.join('downloads','maxvoc_data.csv')
+    info_filename = os.path.join('downloads','maxvoc_info.csv')
+    # save_filename = 'maxvoc_data.csv'
+    # info_filename = 'maxvoc_info.csv'
+    # temp_filename = os.path.join('downloads', session_id, 'maxvoc_temp.pkl')
     print('Saving data as ' + str(save_filename) + '...')
     with open(save_filename,'w') as f:
         f.write(info_df.to_csv(index=False))
