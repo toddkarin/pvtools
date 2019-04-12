ec84661d751ba56a1772195c3cc9d632e7264640
toddkarin <toddkarin@lbl.gov>
2019-04-12 15:00:28 -0700

    update js

--- assets/01_google_analytics.js
+++ assets/01_google_analytics.js
@@ -1,9 +1,6 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138277719-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'UA-138277719-1');
-</script>
+gtag('config', 'UA-138277719-1');
+
